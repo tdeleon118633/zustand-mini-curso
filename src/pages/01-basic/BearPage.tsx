eae6b575552941b6ab9e0ec25e9b757d7295936d
Tito De Leon <compuaserv@gmail.com>
2026-01-07 10:26:23 -0600

    ✨ feat: Reordanamiento de carpetas

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -14,32 +14,9 @@ export const BearPage = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
         <BlackBears />
-
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
-
+        <PolarBears />
+        <PandaBears />
+        <BearsDisplay />
 
       </div>
 
@@ -48,7 +25,7 @@ export const BearPage = () => {
 };
 
 export const BlackBears = () => {
-    const blackBears =  userBearsStore(state => state.blackBears);
+  const blackBears =  userBearsStore(state => state.blackBears);
   const increaseBlackBears =  userBearsStore(state => state.increaseBlackBears);
   return(
         <WhiteCard centered>
@@ -61,5 +38,61 @@ export const BlackBears = () => {
           </div>
 
         </WhiteCard>
+  )
+}
+
+export const PandaBears = () => {
+
+  const pandaBears =  userBearsStore(state => state.pandaBears);
+  const increasePandaBears =  userBearsStore(state => state.increasePandaBears);
+  return(
+        <WhiteCard centered>
+          <h2>Osos Pandas</h2>
+
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => increasePandaBears(+1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+            <button onClick={() => increasePandaBears(-1)}> -1</button>
+          </div>
+
+        </WhiteCard>
+  )
+}
+
+export const PolarBears = () => { 
+
+   const polarBears =  userBearsStore(state => state.polarBears);
+  const increasePolarBears =  userBearsStore(state => state.increasePolarBears);
+  return(
+        <WhiteCard centered>
+          <h2>Osos Negros</h2>
+
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => increasePolarBears(+1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+            <button onClick={() => increasePolarBears(-1)}> -1</button>
+          </div>
+
+        </WhiteCard>
+  )
+
+}
+
+export const BearsDisplay = () => {
+
+  const bears = userBearsStore(state => state.bears);
+  //const bears = userBearsStore(useShallow(state => state.bears)); // NO VUELVE A RENDERIZAR
+  const donNothing = userBearsStore(state => state.donNothing);
+
+  return(
+    <WhiteCard centered>
+      <h2>Todos los Osos</h2>
+      <button onClick={donNothing}>No Hacer Nada</button>
+      <ul>
+        {bears.map(bear => (
+          <li key={bear.id}>{bear.name}</li>
+        ))}
+      </ul>
+    </WhiteCard>
   )
 }
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,18 +1,39 @@
 import {create} from 'zustand';
 
+interface Bear {
+  id:number;
+  name: string;
+}
+
 interface BearState {
   blackBears: number;
   polarBears: number;
   pandaBears: number;
+
+  bears: Bear[];
+
   increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
+
+  donNothing: () => void;
 }
 
 
 
 export const userBearsStore = create<BearState>()((set) => ({
-  blackBears: 10,
+    blackBears: 10,
     polarBears: 5,
     pandaBears: 1,
-  increaseBlackBears: (by:number) => set((state) => ({ blackBears: state.blackBears + by })),
+    bears: [
+      { id: 1, name: 'Black Bear' },
+      { id: 2, name: 'Polar Bear' },
+      { id: 3, name: 'Panda Bear' },
+    ],
+    increaseBlackBears: (by:number) => set((state) => ({ blackBears: state.blackBears + by })),
+    increasePolarBears: (by:number) => set((state) => ({ polarBears: state.polarBears + by })),
+    increasePandaBears: (by:number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+
+    donNothing: () => set((state) => ({ bears: [...state.bears] })),
 }));
 
